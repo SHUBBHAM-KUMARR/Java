c62f00c3e7e570518c413eb95e6b390e1e568022
SHUBBHAM-KUMARR <shubbhammkumarr@outlook.com>
2025-07-13 15:00:53 +0530

    notes and java codes

--- AFirstClass.java
+++ AFirstClass.java
@@ -1,6 +1,5 @@
 public class AFirstClass{
 public static void main(String arg[]){
   System.out.println("Hello World !");
-
 }
 }
---
+++ BVariables.java
@@ -0,0 +1,12 @@
+public class BVariables{
+public static void  main (String args[]){
+ //variable in java
+//  as we know that java is an statically typed language we have to declare the type of variable in java initially unlike js 
+
+int a = 5;
+System.out.println(a);// integers are declared in int type of data
+System.out.println();
+
+}  
+
+}
